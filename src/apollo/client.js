94042afb9ba162544164b37e9c1c81dffd254eb4
Al-mudather <wollof8@gmail.com>
2021-02-17 22:44:04 +0200

    change the router configuration from history to hash

--- src/apollo/client.js
+++ src/apollo/client.js
@@ -13,7 +13,7 @@ if (process.env.NODE_ENV === 'development') {
   uri = 'http://localhost:8000/api/graphql/'
   // uri = 'http://192.168.43.86:8000/api/graphql/'
 } else {
-  uri = 'http://134.209.69.30/api/graphql/'
+  uri = location.origin+'/api/graphql/'
 }
 
 const customLink = ApolloLink.from([
