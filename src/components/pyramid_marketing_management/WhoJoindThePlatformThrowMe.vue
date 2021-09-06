6bdcffefb2d16ee370be8b08462f24f4b29c6ed7
Al-mudather <wollof8@gmail.com>
2021-09-06 04:35:50 +0300

    adding who joind the platforme throw me

--- src/components/pyramid_marketing_management/WhoJoindThePlatformThrowMe.vue
+++ src/components/pyramid_marketing_management/WhoJoindThePlatformThrowMe.vue
@@ -1,18 +1,29 @@
 <template>
  <div class="who-joined-me">
-    <h2><span>203</span>شخص إنضم من خلالي</h2>
+    <h2><span>{{count}}</span>شخص إنضم من خلالي</h2>
   </div>
 </template>
 
 <script>
 
+import {MyPyramidAffiliates} from 'src/queries/pyramid_marketing_management/query/WhoJoindThePlatformThrowMe.js'
+
 export default {
-  name: "WhoJoindThePlatformThrowMe.vue",
+  name: "WhoJoindThePlatformThrowMe",
   data() {
     return {
+      count: 0
     };
   },
   apollo: {
+    myPyramidAffiliates: { 
+      query: MyPyramidAffiliates,
+      result (res) {
+        if (!res.loading) {
+          this.count = res.data.myPyramidAffiliates
+        }
+      }
+    }
   },
   methods: {
   }
