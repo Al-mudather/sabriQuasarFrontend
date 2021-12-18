daadd6cfc18ac0ea426a7a118205c86f896458e0
Al-mudather <wollof8@gmail.com>
2021-12-18 17:11:59 +0300

    fac

--- src/components/Account/FacebookAuthentication.vue
+++ src/components/Account/FacebookAuthentication.vue
@@ -140,9 +140,9 @@ export default {
                     force: true
                 })
                 .then(r => {
-                    console.log("Facebook");
-                    console.log(r);
-                    console.log("Facebook");
+                    // console.log("Facebook");
+                    // console.log(r);
+                    // console.log("Facebook");
 
                     // this.loginAuthMutation(
                     //     r.authResponse.access_token,
@@ -155,7 +155,7 @@ export default {
                         .api(`/me?access_token=${r.authResponse.access_token}`)
                         .then(res => {
                             
-                            var facebook = hello("facebook").getAuthResponse();
+                            var facebook = hello("facebook");
                             console.log("lllllllllllllllll");
                             console.log(facebook);
                             console.log("lllllllllllllllll");
