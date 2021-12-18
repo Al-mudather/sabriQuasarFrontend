86212103e9c9e08f2d80ce8f5e60b591f5b59680
Al-mudather <wollof8@gmail.com>
2021-12-18 20:04:29 +0300

    fac

--- src/components/Account/FacebookAuthentication.vue
+++ src/components/Account/FacebookAuthentication.vue
@@ -14,8 +14,6 @@ import { SocialAuth } from "src/queries/account_management/mutation/CreateSocail
 import { CheckTheUserPermissionToUsePlatforme } from 'src/queries/pyramid_marketing_management/query/CheckPyramidAffiliateQuery'
 import { mapActions } from "vuex";
 
-import * as q_hello from 'hellojs';
-
 export default {
     name: "FacebookAuthentication",
     data () {
@@ -135,54 +133,65 @@ export default {
 
         helloFacebookAuth(network = "facebook") {
 
-            q_hello.init({
-                facebook: '757236575189030'
+            this.$jquery(document).ready( () => {
+                this.$jquery.ajaxSetup({ cache: true });
+                this.$jquery.getScript('https://connect.facebook.net/en_US/sdk.js', () => {
+                    FB.init({
+                        appId: '757236575189030',
+                        version: 'v2.7'
+                    }); 
+                    this.$jquery('#loginbutton,#feedbutton').removeAttr('disabled');
+                    FB.getLoginStatus( (res) => {
+                        console.log('lllllllllllllll')
+                        console.log(res)
+                        console.log('lllllllllllllll')
+                    } );
+                });
             });
 
-            const facebook = q_hello.use("facebook"); 
+            // const hello = this.hello;
 
-            const hello = this.hello;
+            // hello('facebook')
+            // .login({
+            //     scope: "public_profile,email",
+            //     force: true
+            // })
+            // .then(r => {
+            //     // console.log("Facebook");
+            //     // console.log(r);
+            //     // console.log("Facebook");
 
-            facebook
-                .login({
-                    scope: "public_profile,email",
-                    force: true
-                })
-                .then(r => {
-                    // console.log("Facebook");
-                    // console.log(r);
-                    // console.log("Facebook");
+            //     // this.loginAuthMutation(
+            //     //     r.authResponse.access_token,
+            //     //     "facebook",
+            //     //     r.email
+            //     // );
 
-                    // this.loginAuthMutation(
-                    //     r.authResponse.access_token,
-                    //     "facebook",
-                    //     r.email
-                    // );
+            //     // Call user information, for the given network
+            //         // .api(`/me?access_token=${r.authResponse.access_token}`)
+            //     hello('facebook')
+            //     .api(`/me`)
+            //     .then(res => {
 
-                    // Call user information, for the given network
-                    facebook
-                        .api(`/me?access_token=${r.authResponse.access_token}`)
-                        .then(res => {
+            //         var facebookRes = hello('facebook').getAuthResponse();
+            //         // var facebook = hello.getAuthResponse('facebook')
+            //         console.log("lllllllllllllllll");
+            //         console.log(res);
+            //         console.log("lllllllllllllllll");
 
-                            var facebookRes = facebook.getAuthResponse();
-                            // var facebook = hello.getAuthResponse('facebook')
-                            console.log("lllllllllllllllll");
-                            console.log(facebookRes);
-                            console.log("lllllllllllllllll");
+            //         // this.loginAuthMutation(
+            //         //     r.authResponse.access_token,
+            //         //     "facebook",
+            //         //     res.email
+            //         // );
+            //     }, (e) => {
+            //         console.log("eeeeeeeeeeeeeeeeeeeee");
+            //         console.log(e);
+            //         console.log("eeeeeeeeeeeeeeeeeeeee");
+            //     }
 
-                            // this.loginAuthMutation(
-                            //     r.authResponse.access_token,
-                            //     "facebook",
-                            //     res.email
-                            // );
-                        }, (e) => {
-                            console.log("eeeeeeeeeeeeeeeeeeeee");
-                            console.log(e);
-                            console.log("eeeeeeeeeeeeeeeeeeeee");
-                        }
-
-                        );
-                });
+            //     );
+            // });
         }
     }
 };
