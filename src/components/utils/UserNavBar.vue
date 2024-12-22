e7a1041cfeb71aec436a27b4bc1e417367b8a626
Al-mudather <wollof8@gmail.com>
2024-12-22 20:36:42 +0400

    enahance-the-about-course

--- src/components/utils/UserNavBar.vue
+++ src/components/utils/UserNavBar.vue
@@ -1,100 +1,102 @@
 <template>
-    <section class="navv">
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col-lg-12">
-                    <div class="menu" @click="changeMenuState" style="cursor: pointer">
-                        <img src="~assets/img/menu.png" alt="" />
-                    </div>
-                    <div class="user" v-if="lodash.get(user,'[firstName]')">
-                        <img src="~assets/img/man.png" alt="" />
-                        <h3>{{lodash.get(user,'[firstName]')}} {{lodash.get(user,'[lastName]')}}</h3>
-                    </div>
-                    <div class="user" v-else-if="lodash.get(user,'[username]')">
-                        <img src="~assets/img/man.png" alt="" />
-                        <h3>{{lodash.get(user,'[username]')}}</h3>
-                    </div>
-                    <div class="user heading" v-else>
-                        {{lodash.get(user,'[email]')}}
-                    </div>
-                    <div class="notification" @click="$router.push({ name: 'notification' })">
-                        <!-- {{lodash.get(myNotifications,'[totalCount]')}} -->
+  <section class="navv">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-lg-12">
+          <div class="menu" @click="changeMenuState" style="cursor: pointer">
+            <img src="~assets/img/menu.png" alt="" />
+          </div>
+          <div class="user" v-if="lodash.get(user, '[firstName]')">
+            <img src="~assets/img/man.png" alt="" />
+            <h3>
+              {{ lodash.get(user, "[firstName]") }}
+              {{ lodash.get(user, "[lastName]") }}
+            </h3>
+          </div>
+          <div class="user" v-else-if="lodash.get(user, '[username]')">
+            <img src="~assets/img/man.png" alt="" />
+            <h3>{{ lodash.get(user, "[username]") }}</h3>
+          </div>
+          <div class="user heading" v-else>
+            {{ lodash.get(user, "[email]") }}
+          </div>
+          <!-- <div class="notification" @click="$router.push({ name: 'notification' })">
                         <span>+{{lodash.get(myNotifications,'[totalCount]')}}</span>
                         <img src="~assets/img/notif.png" alt="" />
-                    </div>
-                </div>
-            </div>
+                    </div> -->
         </div>
-    </section>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
-import { mapState, mapActions } from 'vuex'
-import { NotificationCreatedSubscription } from 'src/queries/notification_management/subscription/NotificationCreatedSubscription'
-import { GetAllMyNotificationsCount } from 'src/queries/notification_management/query/GetAllMyNotificationsCount'
- 
+import { mapState, mapActions } from "vuex";
+// import { NotificationCreatedSubscription } from 'src/queries/notification_management/subscription/NotificationCreatedSubscription'
+// import { GetAllMyNotificationsCount } from 'src/queries/notification_management/query/GetAllMyNotificationsCount'
+
 export default {
-    name: "CoursesNavBar",
-    data () {
-        return {
-            lodash: this.$_,
-            myNotifications: []
-        }
+  name: "CoursesNavBar",
+  data() {
+    return {
+      lodash: this.$_,
+      myNotifications: [],
+    };
+  },
+
+  // apollo: {
+
+  //   myNotifications: {
+  //     query: GetAllMyNotificationsCount,
+  //     skip () {
+  //       return !this.token
+  //     }
+  //   },
+
+  //   $subscribe: {
+
+  //     notificationCreated: {
+
+  //       query: NotificationCreatedSubscription,
+
+  //       result({data}) {
+  //         if (this.$_.get(this.myNotifications, '[edges]')) {
+  //           this.myNotifications.edges.unshift({
+  //             node: data.notificationCreated.notification
+  //           })
+  //           this.myNotifications.totalCount++
+  //         } else {
+  //           this.myNotifications = {
+  //             totalCount: 1,
+  //             edges: {
+  //               node: data.notificationCreated.notification
+  //             }
+  //           }
+  //         }
+  //       },
+
+  //     },
+
+  //   },
+
+  // },
+
+  // watch: {
+  //   myNotifications (value) {
+  //     this.$root.$emit('NotificationData', value)
+  //   }
+  // },
+  computed: {
+    ...mapState("authentication", ["user", "token"]),
+  },
+
+  methods: {
+    ...mapActions("settings", ["setIsEnglishAction", "setOpenMenuAction"]),
+
+    changeMenuState() {
+      this.setOpenMenuAction(true);
     },
-
-    apollo: {
-
-      myNotifications: {
-        query: GetAllMyNotificationsCount,
-        skip () {
-          return !this.token
-        }
-      },
-
-      $subscribe: {
-
-        notificationCreated: {
-
-          query: NotificationCreatedSubscription,
-
-          result({data}) {
-            if (this.$_.get(this.myNotifications, '[edges]')) {
-              this.myNotifications.edges.unshift({
-                node: data.notificationCreated.notification
-              })
-              this.myNotifications.totalCount++
-            } else {
-              this.myNotifications = {
-                totalCount: 1,
-                edges: {
-                  node: data.notificationCreated.notification
-                }
-              }
-            }
-          },
-
-        },
-
-      },
- 
-    },
-
-    watch: {
-      myNotifications (value) {
-        this.$root.$emit('NotificationData', value)
-      }
-    },
-    computed: {
-      ...mapState('authentication', ['user', 'token'])
-    },
-
-    methods: {
-      ...mapActions('settings', ['setIsEnglishAction', 'setOpenMenuAction']),
-
-      changeMenuState () {
-        this.setOpenMenuAction(true)
-      },
-    }
+  },
 };
 </script>
 <style lang="scss">
@@ -103,8 +105,8 @@ export default {
 @import "src/css/layout/_navbar.scss";
 
 .heading {
-    font-size: 29px;
-    font-family: 'cairoR';
-    color: #7B7B7B;
+  font-size: 29px;
+  font-family: "cairoR";
+  color: #7b7b7b;
 }
 </style>
